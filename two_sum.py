d4cb3d50c094873d4b3b2c50ead2ef440928eaba
zawad17 <abdzawad00007@gmail.com>
2025-07-28 23:10:30 +0600

    two_sum_sorted_array

--- two_sum.py
+++ two_sum.py
@@ -1,11 +1,23 @@
-list = [1, 2, 4, 7, 11, 15]
-target = 15 
-def two_sum(nums, target):
-    num_dict = {}
-    for i, num in enumerate(nums):
-        complement = target - num
-        if complement in num_dict:
-            return [num_dict[complement], i]
-        num_dict[num] = i
+def pair_two_sum(nums, target):
+    left = 0
+    right = len(nums) - 1
+    while (left < right) :
+        current_value = nums[left] + nums[right]
+
+        if current_value == target:
+            return [nums[left], nums[right]]
+        elif nums[left] + nums[right] < target :
+            left +=1
+        else:
+            right -=1
     return []
-print(two_sum(list, target))
+
+nums = [1, 2, 4, 7, 11, 15]
+target = 15
+result = pair_two_sum(nums, target)
+if result:
+    print("Index: ",(nums.index(result[0]), nums.index(result[1])))
+    print("Pair found:", result)
+    
+        
+  
